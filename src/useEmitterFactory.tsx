250bfced9f5c1cf54e3019e1f89707e1966db49b
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-08-29 14:39:34 +0800

    docs: add documentation snippets

--- src/useEmitterFactory.tsx
+++ src/useEmitterFactory.tsx
@@ -10,10 +10,40 @@ type EmitterFactory = <Emitter extends BasicEmitter>(
   dependencies?: DependencyList,
 ) => void;
 
-export interface Emitting<Emitter extends BasicEmitter> {
+/**
+ * An interface for components that emit emitters. Adds an `emitsVia?` prop.
+ *
+ * `extends` this interface on the props interface of the component that
+ * `useEmitterFactory`.
+ *
+ * @example
+ * interface Navigation extends Emits<Navigator> { ... }
+ *
+ * interface Navigator {
+ *   scrollBy: (n: number) => void;
+ *   scrollToTop: () => void;
+ * }
+ */
+export interface Emits<Emitter extends BasicEmitter> {
+  /**
+   * Provide the method that binds this component's emitter to your component.
+   *
+   * @example
+   * import Navigation, { Navigator } from '...';
+   *
+   * const [navigator, setNavigator] = useState<Navigator>();
+   * <Navigation emitsVia={setNavigator} ... />
+   */
   emitsVia?: Binder<Emitter>;
 }
 
+/**
+ * Attaches your emitters after this component's render, just like a factory!
+ * @param props This component's `props`. Must extends `Emits<YourEmitter>`.
+ * @param emitters Implementations of `YourEmitter` to emit to the client.
+ * @param dependencies If present, the factory will only run when values
+ * in this array changes. Otherwise, it will only run once after render.
+ */
 const useEmitterFactory: EmitterFactory = (props, emitters, dependencies) => {
   useEffect(() => {
     props.emitsVia?.(emitters);
